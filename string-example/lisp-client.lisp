38f8ce25c9990e70177807802eca7ce132caabd5
Paul Tarvydas <paultarvydas@gmail.com>
2018-12-05 09:56:06 -0500

    showing cheating-log works, but my-log does not work

--- string-example/lisp-client.lisp
+++ string-example/lisp-client.lisp
@@ -10,21 +10,28 @@
   (str :string)
   (count :int))
 
-(cffi:defcfun ("Hello" hello) :void)
-
-(cffi:defcfun ("Log" cheating-log) :void
-  (str :string)
-  (count :int))
-
-(cffi:defcfun ("Log" my-log) :void
-  (str (:struct go-string)))
+(cffi:defcfun ("Hello" hello) 
+    :void)
 
 ;; a go string is a C struct, "abc" ---> { "abc", 3 }
 
+(cffi:defcfun ("Log" cheating-log) ;; uses pass-by-ref only
+    :void
+  (str :string)
+  (count :int))
+
+(cffi:defcfun ("Log" my-log) ;; uses pass-by-value of struct
+    :void
+  (str (:struct go-string)))
+
+
 (defun main (argv)
   (declare (ignore argv))
-  (cffi:load-foreign-library :golib)
-  (hello)
-  (cheating-log "Hello, cheating, from Lisp" 26)
-  (my-log "Hello from Lisp"))
+  (let ((msg1 "Hello, cheating, from Lisp")
+	(msg2 "Hello, ////////, from Lisp"))
+    (let ((len1 (length msg1)))
+      (cffi:load-foreign-library :golib)
+      (hello)
+      (cheating-log msg1 len1)
+      (my-log msg2))))
 
